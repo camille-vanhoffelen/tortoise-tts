519732f107604002c88d28a9204acc98453ed610
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-10-15 17:30:55 +0200

    feat: upped transformers back to 4.32.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         'unidecode',
         'scipy',
         'librosa',
-        'transformers==4.31.0',
+        'transformers==4.32.1',
         'tokenizers',
     ],
     classifiers=[
